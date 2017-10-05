ce1975b61ea74ace1a5e00a4c258aef42d967a0c
Paul Webanck <paul.webanck@edhec.com>
2017-10-05 18:53:29 +0200

    routes deleted to avoid conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-
-  get 'restaurants/show'
-
   devise_for :restaurants
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
