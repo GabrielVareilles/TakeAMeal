c0c21ff2c3a2a8cd838293250ffb7a61f9964d8a
Julien Corbin <corbin.julien@gmail.com>
2017-10-05 17:08:27 +0200

    added show and index views, defined corresponding methods

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,12 +1,17 @@
 class MealsController < ApplicationController
-
+  before_action :find_cocktail, only: [ :show ]
   def index
+    @meals = Meal.all
   end
 
   def show
   end
 
   private
+
+  def find_meal
+    @meal = Meal.find(params[:id])
+  end
 
   def meal_params
     params.require(:meal).permit(:name, :description, :photo, :photo_cache)
