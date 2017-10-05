dd456e15fe02560781a4a5e071a556813cdb5113
Julien Corbin <corbin.julien@gmail.com>
2017-10-05 18:43:23 +0200

    started to write routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :restaurants
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :show, :index ]
+  resources :restaurants do
+    resources :meals, only: [ :index, :show ]
+    resources :meals do
+      resources :orders, only: [ :new ]
+    end
+  end
+  resources :meals, only: [ :index ]
+
 end
