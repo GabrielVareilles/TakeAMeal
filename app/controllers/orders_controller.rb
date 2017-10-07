1b958a400b39bc6037915005ce309de0847898e0
Paul Webanck <paul.webanck@edhec.com>
2017-10-07 16:33:56 +0200

    meals/show added, modified controllers and stuff to generate the order creation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,37 @@
 class OrdersController < ApplicationController
+  before_action :find_order, only: [ :show, :update ]
 
   def new
+    @order = Order.new
   end
 
   def create
+    @meal = Meal.find(params[:meal_id])
+    @order = current_user.orders.new(order_params)
+    @order.meal =  @meal
+    if @order.save
+      flash[:notice] = "Votre commande a été créée"
+      redirect_to @meal
+    else
+      render 'meals/show'
+    end
   end
 
   def update
+    @order.status = "canceled"
+    @order.save!
   end
+
+  def edit
+  end
+
+  private
+
+    def find_order
+      @order = Order.find(params[:id])
+    end
+
+   def order_params
+     params.require(:order).permit(:pick_up_time)
+   end
 end
