aed08d260deb51193fb2a91e41c146adbbdd8b8a
Julien Corbin <corbin.julien@gmail.com>
2017-10-07 14:00:56 +0200

    suppression des doublons

--- db/migrate/20171005135422_add_fields_to_restaurants.rb
+++ db/migrate/20171005135422_add_fields_to_restaurants.rb
@@ -3,7 +3,6 @@ class AddFieldsToRestaurants < ActiveRecord::Migration[5.1]
     add_column :restaurants, :name, :string
     add_column :restaurants, :address, :string
     add_column :restaurants, :address2, :string
-    add_column :restaurants, :email_address, :string
     add_column :restaurants, :phone_number, :string
     add_column :restaurants, :category, :string
     add_column :restaurants, :max_meal, :integer
