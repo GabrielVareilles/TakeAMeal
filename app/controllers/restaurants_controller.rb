d238d8969f136deed278b0ca3f1a086e57ad1fc7
Paul Webanck <paul.webanck@edhec.com>
2017-10-10 12:26:17 +0200

    work on pundit for restaurant but not available yet

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,17 @@
 class RestaurantsController < ApplicationController
+  include Pundit
+  skip_before_action :authenticate_user!, raise: false
+
   before_action :find_restaurant, only: [ :show, :update, :edit]
+  before_action :authenticate_restaurant!, only: :show
+
+  after_action :verify_authorized, only: :show, unless: :skip_pundit?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(root_path)
+  end
 
   def index
     @restaurants = Restaurant.all
@@ -9,8 +21,11 @@ class RestaurantsController < ApplicationController
     #where the restaurant can see its orders of the day and set the maximum amount of meals per day
     #implement in index & show how many meals left.
     #valeur par défaut pour l'instant
+    # authorize @restaurant
     @meal = Meal.where(restaurant_id: @restaurant.id).first
     @orders = Order.where(meal_id: @meal.id)
+
+    authorize @restaurant
   end
 
   def update
@@ -28,5 +43,4 @@ class RestaurantsController < ApplicationController
   def find_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
-
 end
