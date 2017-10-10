cf73c9be289fc8cc111aa7f742e525335cce9fd9
Paul Webanck <paul.webanck@edhec.com>
2017-10-10 13:48:12 +0200

    conflict resolution whithout end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
     else
       restaurants_path
   end
-  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
