4d424163ba05d035fe342dc4f5176973e5ea4a70
Julien Corbin <corbin.julien@gmail.com>
2017-10-12 19:52:19 +0200

    removed ALL my stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,2 @@
 module ApplicationHelper
-  def has_subscription?
-    unless current_user.blank?
-      @last_order = SubscriptionOrder.where(user_id: current_user.id).last
-      if @last_order == nil
-        return @last_order
-      elsif @last_order.end_date >= Date.today
-        return true
-      else
-        return false
-      end
-    end
-  end
 end
